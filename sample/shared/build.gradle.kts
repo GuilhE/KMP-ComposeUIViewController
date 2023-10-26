5209e3fd04b465850c0ff0bb14b3b9cb33f313d2
GuilhE <gui.delgado@hotmail.com>
2023-10-26 14:04:55 +0100

    Bumps Compose-jb

--- sample/shared/build.gradle.kts
+++ sample/shared/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.ksp)
-    id("org.jetbrains.compose") version "1.5.10-rc01"
+    id("org.jetbrains.compose") version "1.5.10-rc02"
 }
 
 compose {
