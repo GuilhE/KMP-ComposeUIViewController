0418371c81ec06e029d9cefc22adb65585556528
GuilhE <gui.delgado@hotmail.com>
2023-11-02 10:16:35 +0000

    Sample build.gradle clean up

--- sample/shared/build.gradle.kts
+++ sample/shared/build.gradle.kts
@@ -10,33 +10,19 @@ compose {
 
 kotlin {
     jvm()
-    val iosX64 = iosX64()
-    val iosArm64 = iosArm64()
-    val iosSimulatorArm64 = iosSimulatorArm64()
     applyDefaultHierarchyTemplate()
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material3)
-                implementation(compose.ui)
-            }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
         }
+        jvmMain.dependencies { implementation(compose.preview) }
+        iosMain.dependencies { implementation(libs.composeuiviewcontroller.annotations) }
 
-        val jvmMain by getting {
-            dependencies {
-                implementation(compose.preview)
-            }
-        }
-
-        val iosMain by getting {
-            dependencies {
-                implementation(libs.composeuiviewcontroller.annotations)
-            }
-        }
-        listOf(iosX64, iosArm64, iosSimulatorArm64).forEach { target ->
+        listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach { target ->
             target.binaries.framework { baseName = "SharedComposables" }
             val targetName = target.name.replaceFirstChar { it.uppercaseChar() }
             dependencies.add("ksp$targetName", libs.composeuiviewcontroller.ksp)
